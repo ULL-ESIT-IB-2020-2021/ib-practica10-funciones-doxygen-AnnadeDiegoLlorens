e9f47f83f23c06a090d0691d166ae15f2ff2fe57
Anna de Diego Llorens <alu0101441320@ull.edu.es>
2020-12-14 00:52:25 +0000

    carpeta cripto

---
+++ cripto/cripto.cc
@@ -0,0 +1,11 @@
+# include <iostream>
+# include <fstream>
+# include <string>
+
+using namespace std; 
+
+int main (int argc, char* argv[]){
+    string arch_entrada = argv[1]
+    ifstream archivo_de_entrada {arch_entrada}, archivo_de_salida{argv[2]};
+
+}
